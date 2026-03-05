231b65efe4f8c5ebf26a164be0abf89ba397e054
Alexander Asomba <3323537+alexasomba@users.noreply.github.com>
2026-03-05 13:04:11 +0100

    chore: update .gitignore and clean up artifacts

--- src/middleware.d.ts
+++
@@ -1,5 +0,0 @@
-import type { PaystackClientLike, PaystackOptions } from "./types";
-export declare const referenceMiddleware: (options: PaystackOptions<PaystackClientLike>, action: "initialize-transaction" | "verify-transaction" | "list-subscriptions" | "list-transactions" | "disable-subscription" | "enable-subscription" | "get-subscription-manage-link") => (inputContext: better_call0.MiddlewareInputContext<Options>) => Promise<{
-    referenceId: string;
-}>;
-//# sourceMappingURL=middleware.d.ts.map
